5828c719cb9d9a9bdf82760b48317b892a7d7287
= <zainr7989@gmail.com>
2019-10-21 15:32:56 -0700

    Added rearrange module

--- Code/python_quote.py
+++ Code/python_quote.py
@@ -1,8 +1,8 @@
 import random
 
-quotes = ("It's just a flesh wound.",
+quotes = ["It's just a flesh wound.",
           "He's not the Messiah. He's a very naughty boy!",
-          "THIS IS AN EX-PARROT!!")
+          "THIS IS AN EX-PARROT!!"]
 
 
 def random_python_quote():
