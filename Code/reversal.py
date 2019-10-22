bcaa6d5a16192908bee25c48db1f94c89054759d
= <zainr7989@gmail.com>
2019-10-22 09:38:46 -0700

    [ADD] Nearly complete module for reversing words and sentences.

---
+++ Code/reversal.py
@@ -0,0 +1,49 @@
+import sys
+
+
+def go_to_next_line():
+    """Helper function to make console go to next line, at the end of
+       reverse() execution.
+    """
+    print("")
+
+
+def reverse(input_str):
+    """Returns the input string in reverse.
+       Param: input_str(str): can be either a single word or a sentence
+    """
+    # if input_str is a sentence, then outputs words in sentence in reverse
+    space = " "
+    if space in input_str:
+        sentence = list()
+        for word in input_str:
+            sentence.append(word)
+            sentence.append(space)
+
+        # output reverse sentence
+        sentence_length = len(sentence)
+        for i in range(sentence_length):
+            last_word_index = sentence_length - (i + 1)
+            last_word = sentence[last_word_index]
+            print(last_word, end=space)
+
+        go_to_next_line()
+
+    # if input_str is a single word, then outputs letters in reverse
+    else:
+        word = list()
+        for letter in input_str:
+            word.append(letter)
+
+        # output word in reverse
+        word_length = len(word)
+        for i in range(word_length):
+            letter_from_end_index = i + 1
+            letter_from_end = word[word_length - letter_from_end_index]
+            print(letter_from_end, end="")
+
+        go_to_next_line()
+
+
+if __name__ == "__main__":
+    reverse(sys.argv[1:])
