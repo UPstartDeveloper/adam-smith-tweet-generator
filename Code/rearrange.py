25fefcf9a1ab611778846e2239424ce2082692a5
= <zainr7989@gmail.com>
2019-10-23 15:38:48 -0700

    [FIX] Inifinite loop, allow for multiple instances of same word.

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -9,11 +9,12 @@ def rearrange(words):
     """
     # create a list storing str from words in a randomly decided order
     rearranged_words = list()
-    while not len(rearranged_words) == len(words):
+    while not len(words) == 0:
         rand_index = random.randint(0, len(words) - 1)
         random_word = words[rand_index]
-        if random_word not in rearranged_words:
+        if random_word not in rearranged_words or len(words) > 0:
             rearranged_words.append(random_word)
+            words.remove(random_word)
 
     # show the order of str in rearranged_words
     for word in rearranged_words:
