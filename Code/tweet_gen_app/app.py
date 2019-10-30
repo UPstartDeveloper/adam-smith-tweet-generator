4cd74020a38ab4c40658dfdc4a6296f13920aed2
= <zainr7989@gmail.com>
2019-10-30 16:38:03 -0700

    [UPDATE] Project files to simplify importing modules.

--- Code/tweet_gen_app/app.py
+++ Code/tweet_gen_app/app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template
-import sys
-sys.path.append('../')
 from histogram import histogram
 from stochastic_sampling import stochastic_sample
 
-
 # importing corpus text and creating a histogram
-corpus = "../adam_smith.txt"
+corpus = "adam_smith.txt"
 histo = histogram(corpus)
 # Flask app for tweet generator
 app = Flask(__name__)
