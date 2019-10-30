5a043045acae2a559651f2f08c5f945f88216141
= <zainr7989@gmail.com>
2019-10-30 16:49:26 -0700

    [FIX] Non-capitalization of first word from sentence.

--- Code/tweet_gen_app/app.py
+++ Code/tweet_gen_app/app.py
@@ -9,12 +9,26 @@ histo = histogram(corpus)
 app = Flask(__name__)
 
 
+def capitalize_first_word(words):
+    """Capitalize the word letter of a string.
+       Param: words(list): str to go in sentence
+       Return: modified_words(list): str where first str is capitalized
+    """
+    first_word = words[0]
+    first_letter = first_word[0].upper()
+    rest_of_word = first_word[1:]
+    reassigned_word = first_letter + rest_of_word
+    words[0] = reassigned_word
+    return words
+
+
 @app.route("/")
 def index():
     """Display a single generated word on each reload."""
     words = list()
     for i in range(10):
         words.append(stochastic_sample(histo))
+    words = capitalize_first_word(words)
     return render_template("index.html", words=words)
 
 
