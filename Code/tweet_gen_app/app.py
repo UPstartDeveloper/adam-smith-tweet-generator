5c1a1b0ba50b4b541bc70a5baf234752118c308b
= <zainr7989@gmail.com>
2019-11-04 16:22:46 -0800

    [ADD] Web files to submodule repo.

--- Code/tweet_gen_app/app.py
+++ Code/tweet_gen_app/app.py
@@ -22,9 +22,10 @@ def capitalize_first_word(words):
     return words
 
 
+"""
 @app.route('/')
 def index():
-    """Display a form to see a sentence."""
+    '''Display a form to see a sentence.'''
     return render_template("index.html")
 
 
@@ -61,7 +62,7 @@ def index():
 def reload():
     '''Redirect user to a new load of the home page.'''
     return redirect(url_for("index"))
-"""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
