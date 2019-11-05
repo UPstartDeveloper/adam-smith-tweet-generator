c71181c050d9600d5ee8c55800e0fd5294179219
= <zainr7989@gmail.com>
2019-11-05 10:08:05 -0800

    [FIX] Indent in get_clean_words.

--- Code/tweet_gen_app/histogram.py
+++ Code/tweet_gen_app/histogram.py
@@ -11,25 +11,25 @@ def get_clean_words(file_name):
     with open(file_name, "r") as file:
         # make a list ofd words, contains non alphabetic chars
         words = file.read().split()
-        # remove all occurences of non-alpha chars from data
-        clean_words = []
-        for word in words:
-            clean_word = ([char for char in word if not (
-                char == "." or
-                char == "?" or
-                char == "!" or
-                char == "," or
-                char == ":" or
-                char == ";" or
-                char == "(" or
-                char == ")"
-            )])
-            clean_words.append(clean_word)
-        # make a list of whole words only containing letters
-        clean_words_as_str = []
-        for list_of_chars in clean_words:
-            whole_word = ""
-            clean_words_as_str.append(whole_word.join(list_of_chars))
+    # remove all occurences of non-alpha chars from data
+    clean_words = []
+    for word in words:
+        clean_word = ([char for char in word if not (
+            char == "." or
+            char == "?" or
+            char == "!" or
+            char == "," or
+            char == ":" or
+            char == ";" or
+            char == "(" or
+            char == ")"
+        )])
+        clean_words.append(clean_word)
+    # make a list of whole words only containing letters
+    clean_words_as_str = []
+    for list_of_chars in clean_words:
+        whole_word = ""
+        clean_words_as_str.append(whole_word.join(list_of_chars))
 
     return clean_words_as_str
 
