85ba5ef0550930f7da1ca27837318e2c4db22f9f
= <zainr7989@gmail.com>
2019-11-05 12:49:11 -0800

    [ADD] Refactor add_count method and implement frequency method.

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -20,11 +20,12 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
-        word += count
+        self.get(word) += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        return self.get(word)
 
 
 def print_histogram(word_list):
