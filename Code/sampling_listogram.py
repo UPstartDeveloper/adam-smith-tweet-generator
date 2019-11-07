29bfd7897999de47c071144e14318f628ff54e42
= <zainr7989@gmail.com>
2019-11-07 11:16:57 -0800

    [ADD] Helpers for choosing the dart.

--- Code/sampling_listogram.py
+++ Code/sampling_listogram.py
@@ -40,6 +40,48 @@ def fit_in_range(listogram, index, dart):
                 return index_after
 
 
+def find_size_of_range(low_end, high_end):
+    """Return size of the range."""
+    return high_end - low_end
+
+
+def store_range_size(listogram, index, list_of_sizes):
+    """Store the difference between the high end and low end of the range
+       associated with a word in a list.
+       Param: listogram(Listogram),
+              index(int),
+              list_of_sizes(list)
+       Return: modified list_of_sizes
+    """
+    low_end = listogram[index][1][0]
+    high_end = listogram[index][1][1]
+    size_of_range_of_word = find_size_of_range(low_end, high_end)
+    list_of_sizes.append(size_of_range_of_word)
+    return list_of_sizes
+
+
+def compare_ranges(listogram, index, other_index):
+    """Determine which word has the larger range which dart falls within.
+       If they are of equal size, then the word associated with index returns.
+       Param: listogram(Listogram),
+              index(int),
+              other_index(int)
+       Return: word(str)
+    """
+    range_sizes = list()  # stores the difference between the low and high ends
+    # store the range size of one word
+    low_end = listogram[index][1][0]
+    high_end = listogram[index][1][1]
+    size_of_range_of_word = find_size_of_range(low_end, high_end)
+    range_sizes.append(size_of_range_of_word)
+    # store the range size of the other word
+    low_end = listogram[other_index][1][0]
+    high_end = listogram[other_index][1][1]
+    size_of_range_of_word = find_size_of_range(low_end, high_end)
+    range_sizes.append(size_of_range_of_word)
+    # make the choice, find the word which belongs with the choice
+
+
 def choose_word(listogram, dart):
     """Return the word in the listogram whose associated tuple contains the
        dart, such that list[0] <= dart <= list[1],
@@ -60,11 +102,12 @@ def choose_word(listogram, dart):
         for index in range(len(listogram)):
             # decide if the word at this index has the range containing dart
             word = listogram[index][0]
-            if fit_in_range(listogram, index, dart) is True:
+            does_fit = fit_in_range(listogram, index, dart)
+            if does_fit is True:
                 return word
             else:
                 # compare ranges of the two words dart falls between
-                pass
+                index_of_other_word = does_fit
 
 
 def weighted_sample(listogram):
