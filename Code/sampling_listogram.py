3fdebce78a6cca196942d36f7c2705baaddc93c4
= <zainr7989@gmail.com>
2019-11-07 09:37:13 -0800

    [ADD] make_range use for the listogram structure.

--- Code/sampling_listogram.py
+++ Code/sampling_listogram.py
@@ -2,7 +2,7 @@ import tweet_gen_app.stochastic_sampling as sampler_helper
 import random
 
 
-def calculate_length_of_source(histogram):
+def calculate_length_of_source(listogram):
     """Return the total number of tokens in the histogram."""
     tokens = 0
     for list in histogram:
@@ -10,12 +10,22 @@ def calculate_length_of_source(histogram):
     return tokens
 
 
-def weighted_sample(histogram):
+def weighted_sample(listogram):
     """Chooses a random word from the histogram of word frequency.
        Words that have higher frequnecy counts in the histogram will
        be chosen more often by this function.
-       Param: histogram(list of lists)
+       Param: listogram(list of lists)
        Return: word(str)
     """
     # set up needed values
-    length_of_text = calculate_length_of_source(histogram)
+    length_of_text = calculate_length_of_source(listogram)
+    probability_factor = sampler_helper.calculate_factor(length_of_text)
+    probability = 0
+    # reassign word counts in listogram to ranges of values that dart can equal
+    for list in listogram:
+        current_value = list[1]
+        current_value = sampler_helper.make_range(probability,
+                                                  probability_factor,
+                                                  current_value)
+        probability = current_value[1]
+    print(listogram)
