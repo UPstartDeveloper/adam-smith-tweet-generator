7203f879ca7f1826d26d8cc59d37c0ba2e6be919
= <zainr7989@gmail.com>
2019-11-07 09:19:13 -0800

    [ADD] calculate_length_of_source as a helper.

--- Code/sampling_listogram.py
+++ Code/sampling_listogram.py
@@ -2,6 +2,14 @@ import tweet_gen_app.stochastic_sampling as sampler_helper
 import random
 
 
+def calculate_length_of_source(histogram):
+    """Return the total number of tokens in the histogram."""
+    tokens = 0
+    for list in histogram:
+        tokens += list[1]
+    return tokens
+
+
 def weighted_sample(histogram):
     """Chooses a random word from the histogram of word frequency.
        Words that have higher frequnecy counts in the histogram will
@@ -9,4 +17,5 @@ def weighted_sample(histogram):
        Param: histogram(list of lists)
        Return: word(str)
     """
-    pass
+    # set up needed values
+    length_of_text = calculate_length_of_source(histogram)
