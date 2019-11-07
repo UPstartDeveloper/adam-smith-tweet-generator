0c3a1b1acbb198dfc242ca775e39cbe793a2874b
= <zainr7989@gmail.com>
2019-11-07 09:02:03 -0800

    [ADD] Module to store sampling function for listogram (and its helpers.

---
+++ Code/sampling_listogram.py
@@ -0,0 +1,10 @@
+from tweet_gen_app.stochastic_sampling import *
+
+
+def weighted_sample(histogram):
+    """Chooses a random word from the histogram of word frequency.
+       Words that have higher frequnecy counts in the histogram will
+       be chosen more often by this function.
+       Param: histogram(list of lists)
+       Return: word(str)
+    """
