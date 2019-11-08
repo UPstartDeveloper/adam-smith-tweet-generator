9d263ce94a4f8e7df7a25ecd56858baa14e3021d
= <zainr7989@gmail.com>
2019-11-07 19:24:59 -0800

    [ADD] Provide a words_list property.

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -2,4 +2,6 @@ import tweet_gen_app.clean_words as cw
 
 
 class MarkovChain:
-    pass
+    def __init__(self, words_list=None):
+        if words_list is None:
+            self.words_list = cw.get_clean_words  # use adam smith corpus
