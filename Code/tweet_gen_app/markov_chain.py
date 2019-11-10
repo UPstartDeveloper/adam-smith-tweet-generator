1c5bafedb6148471a6497916e3b21449b976decf
= <zainr7989@gmail.com>
2019-11-10 10:17:29 -0800

    [FIX] Refactor init method to improve time efficiency.

--- Code/tweet_gen_app/markov_chain.py
+++ Code/tweet_gen_app/markov_chain.py
@@ -8,6 +8,30 @@ class MarkovChain:
         """Construct a Markov Chain model.
            Param: words_list(list of str)
         """
+        # Initialize an empty dictionary
+        self.chain = dict()
+        # use the passed in list of words
+        if words_list is not None:
+            self.words_list = words_list
+        else:
+            # use the Adam Smith corpus
+            self.words_list = clean_words.get_clean_words()
+        # populate the Markov Chain
+        for i in range(len(self.words_list)):
+            # avoid IndexError at the end of the list
+            try:
+                state = self.words_list[i]
+                token_after = self.words_list[i + 1]
+                # create a word frequency dict to go along with each state
+                if self.chain.get(state, None) is None:
+                    self.chain[state] = Dictogram([token_after])
+                # if the state already exists, add the token and count
+                else:
+                    self.chain[state].add_count(token_after)
+            # break out of the loop at the end of the list
+            except IndexError:
+                break
+        '''
         # create a list of words from the corpus
         self.words_list = list()
         if words_list is None:
@@ -32,7 +56,7 @@ class MarkovChain:
                 if not index == 0 and word_before == type:
                     tokens_that_follow.append(word)
             self.states[type] = Dictogram(tokens_that_follow)
-
+        '''
     def random_walk(self, length=10):
         """Generate a sentence by randomly transitioning between states.
            Param: length(int) the number of words that should be generated
