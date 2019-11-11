3063dcceb73ebc59192c60d0d9ac139b01035652
= <zainr7989@gmail.com>
2019-11-11 08:54:04 -0800

    [FIX] Use while loop instead of try except in __init__.

--- Code/tweet_gen_app/markov_chain.py
+++ Code/tweet_gen_app/markov_chain.py
@@ -17,46 +17,18 @@ class MarkovChain:
             # use the Adam Smith corpus
             self.words_list = clean_words.get_clean_words()
         # populate the Markov Chain
-        for i in range(len(self.words_list)):
-            # avoid IndexError at the end of the list
-            try:
-                state = self.words_list[i]
-                token_after = self.words_list[i + 1]
-                # create a word frequency dict to go along with each state
-                if self.chain.get(state, None) is None:
-                    self.chain[state] = Dictogram([token_after])
-                # if the state already exists, add the token and count
-                else:
-                    self.chain[state].add_count(token_after)
-            # break out of the loop at the end of the list
-            except IndexError:
-                break
-        '''
-        # create a list of words from the corpus
-        self.words_list = list()
-        if words_list is None:
-            # using adam smith corpus
-            self.words_list = clean_words.get_clean_words()
-        else:
-            # use the passed in list of words
-            self.words_list = words_list
-        # keys in the self.chain dict
-        self.types_of_words = list()
-        self.types_of_words = (
-            [word for word in self.words_list if word not
-             in self.types_of_words]
-        )
-        # create a dict to store all types and their state histograms
-        self.chain = dict()
-        for type in self.types_of_words:
-            tokens_that_follow = list()
-            for index in range(len(self.words_list)):
-                word = self.words_list[index]
-                word_before = self.words_list[index - 1]
-                if not index == 0 and word_before == type:
-                    tokens_that_follow.append(word)
-            self.chain[type] = Dictogram(tokens_that_follow)
-        '''
+        i = 0
+        while i < len(self.words_list) - 1:  # avoiding IndexError
+            state = self.words_list[i]
+            token_after = self.words_list[i + 1]
+            # create a word frequency dict to go along with each state
+            if self.chain.get(state, None) is None:
+                self.chain[state] = Dictogram([token_after])
+            # if the state already exists, add the token and count
+            else:
+                self.chain[state].add_count(token_after)
+            i += 1
+
     def random_walk(self, length=10):
         """Generate a sentence by randomly transitioning between states.
            Param: length(int) the number of words that should be generated
