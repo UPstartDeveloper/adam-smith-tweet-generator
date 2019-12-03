36169e0b64afeeb91ecd47bb7cc37b4dd54ff641
= <zainr7989@gmail.com>
2019-12-02 18:09:20 -0800

    [ADD] enqueue and dequeue methods.

--- Code/tweet_gen_app/higher_order.py
+++ Code/tweet_gen_app/higher_order.py
@@ -5,11 +5,30 @@ import random
 
 
 class HigherMarkovChain(MarkovChain):
-    pass
+    def __init__(self, words_list=None):
+        """Extends all the properties of a First Order MarkovChain.
+           Adds a queue property for calculating probabilities for state
+           transitions.
+
+        """
+        super().__init__(words_list)  # use a first order Markov Chain to start
+        self.queue = list()
+
+    def enqueue(self, item):
+        '''Add the item to the end of the current queue.'''
+        self.queue.append(item)
+
+    def dequeue(self):
+        '''Return the first item in the queue. Raises if there are no items.'''
+        if not len(self.queue) == 0:
+            item = self.queue.pop(0)
+            return item
+        else:
+            raise IndexError('There are currently no items in the queue.')
 
 
 if __name__ == "__main__":
     left_right_list = ['I', 'went', 'left', 'you', 'went', 'right',
                        'I', 'went', 'left', 'I', 'went', 'right']
-    mark = MarkovChain(left_right_list)
+    mark = HigherMarkovChain(left_right_list)
     print(mark.random_walk())
