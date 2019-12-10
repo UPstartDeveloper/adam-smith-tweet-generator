7c0163b3e4de08b2b3a76669e8e3fc777918a7f8
= <zainr7989@gmail.com>
2019-12-10 15:17:25 -0800

    [UPDATE] Load environment variable values.

--- Code/tweet_gen_app/twitter.py
+++ Code/tweet_gen_app/twitter.py
@@ -1,1 +1,9 @@
 import os
+import dotenv
+# load API tokens in from environment variables
+dotenv.load_dotenv('.env')
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
