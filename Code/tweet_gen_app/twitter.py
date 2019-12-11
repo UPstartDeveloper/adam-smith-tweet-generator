1542fa7f55f3a91e15252338d826a41dd27e5f47
= <zainr7989@gmail.com>
2019-12-11 13:52:23 -0800

    [UPDATE] Nest variables into function scope.

--- Code/tweet_gen_app/twitter.py
+++ Code/tweet_gen_app/twitter.py
@@ -4,26 +4,6 @@ import requests
 import tweepy
 
 
-# load API tokens in from environment variables
-dotenv.load_dotenv('.env')
-
-consumer_key = (
-    os.environ.get('TWITTER_CONSUMER_KEY')).encode('utf-8')
-consumer_secret = (
-    os.environ.get('TWITTER_CONSUMER_SECRET'))
-access_token = (
-    os.environ.get('TWITTER_ACCESS_TOKEN'))
-access_token_secret = (
-    os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-    )
-
-# create a new authorized session
-
-# using OAuth2
-session = tweepy.OAuthHandler(consumer_key, consumer_secret)
-session.set_access_token(access_token, access_token_secret)
-authenticated = tweepy.API(session, wait_on_rate_limit=True)
-
 # The URL endpoint to update a status (i.e. tweet)
 url = 'https://api.twitter.com/1.1/statuses/update.json'
 
@@ -42,6 +22,25 @@ def tweet(status_update):
        Returns:
        None
     """
+    # load API tokens in from environment variables
+    dotenv.load_dotenv('.env')
+
+    consumer_key = (
+        os.environ.get('TWITTER_CONSUMER_KEY')).encode('utf-8')
+    consumer_secret = (
+        os.environ.get('TWITTER_CONSUMER_SECRET'))
+    access_token = (
+        os.environ.get('TWITTER_ACCESS_TOKEN'))
+    access_token_secret = (
+        os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+        )
+
+    # create a new authorized session using OAuth2
+    session = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    session.set_access_token(access_token, access_token_secret)
+    authenticated = tweepy.API(session, wait_on_rate_limit=True)
+
+    # send the request to Twitter API
     authenticated.update_status(status=status_update)
 
 
