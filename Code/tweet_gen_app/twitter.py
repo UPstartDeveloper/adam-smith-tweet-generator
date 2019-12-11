9fa8a4f1495b6fd6283065a1815a298aca41848d
= <zainr7989@gmail.com>
2019-12-11 12:05:40 -0800

    [UPDATE] Try using Tweepy module.

--- Code/tweet_gen_app/twitter.py
+++ Code/tweet_gen_app/twitter.py
@@ -1,23 +1,27 @@
 import os
 import dotenv
 import requests
-from requests_oauthlib import OAuth1Session
+import tweepy
 
 
 # load API tokens in from environment variables
 dotenv.load_dotenv('.env')
 
-consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
-consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
-access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+consumer_key = (
+    os.environ.get('TWITTER_CONSUMER_KEY').encode('utf-8'))
+consumer_secret = (
+    os.environ.get('TWITTER_CONSUMER_SECRET').encode('utf-8'))
+access_token = (
+    os.environ.get('TWITTER_ACCESS_TOKEN').encode('utf-8'))
+access_token_secret = (
+    os.environ.get('TWITTER_ACCESS_TOKEN_SECRET').encode('utf-8'))
 
 # create a new authorized session
-session = OAuth1Session(consumer_key,
-                        client_secret=consumer_secret,
-                        resource_owner_key=access_token,
-                        resource_owner_secret=access_token_secret)
 
+# using OAuth2
+session = tweepy.OAuthHandler(consumer_key, consumer_secret)
+session.set_access_token(access_token, access_token_secret)
+authenticated = tweepy.API(session)
 
 # The URL endpoint to update a status (i.e. tweet)
 url = 'https://api.twitter.com/1.1/statuses/update.json'
@@ -25,8 +29,7 @@ url = 'https://api.twitter.com/1.1/statuses/update.json'
 # The contents of status (i.e. tweet text)
 status = 'If you are reading this on Twitter, the API request worked!'
 
-# Send a POST request to the url with a 'status' parameter
-resp = session.post(url, {'status': status})
+authenticated.update_status(status=status)
 
 # Show the text from the response
-print(resp.text)
+print(status)
