11959d74b001221aac13bf16e6a5af49b2df2970
= <zainr7989@gmail.com>
2019-12-11 12:17:12 -0800

    [FIX] Twitter status updates.

--- Code/tweet_gen_app/twitter.py
+++ Code/tweet_gen_app/twitter.py
@@ -8,28 +8,28 @@ import tweepy
 dotenv.load_dotenv('.env')
 
 consumer_key = (
-    os.environ.get('TWITTER_CONSUMER_KEY').encode('utf-8'))
+    os.environ.get('TWITTER_CONSUMER_KEY'))
 consumer_secret = (
-    os.environ.get('TWITTER_CONSUMER_SECRET').encode('utf-8'))
+    os.environ.get('TWITTER_CONSUMER_SECRET'))
 access_token = (
-    os.environ.get('TWITTER_ACCESS_TOKEN').encode('utf-8'))
+    os.environ.get('TWITTER_ACCESS_TOKEN'))
 access_token_secret = (
-    os.environ.get('TWITTER_ACCESS_TOKEN_SECRET').encode('utf-8'))
+    os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+    )
 
 # create a new authorized session
 
 # using OAuth2
 session = tweepy.OAuthHandler(consumer_key, consumer_secret)
 session.set_access_token(access_token, access_token_secret)
-authenticated = tweepy.API(session)
+authenticated = tweepy.API(session, wait_on_rate_limit=True)
 
 # The URL endpoint to update a status (i.e. tweet)
 url = 'https://api.twitter.com/1.1/statuses/update.json'
 
 # The contents of status (i.e. tweet text)
-status = 'If you are reading this on Twitter, the API request worked!'
+status = 'Woohoo!'
 
 authenticated.update_status(status=status)
 
 # Show the text from the response
-print(status)
